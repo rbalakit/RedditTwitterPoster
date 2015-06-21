dd589a3df84532420e5083de561752c29508f4e8
rbalakit <rbalakit@gmail.com>
2015-06-21 10:59:08 -0500

    Repost Prevention, Reformatting, and proper bools
    I learned that python code should follow a certain style, so I
    reformatted all the code with autopep8.
    
    replaced the "enabled"/"disabled" options with proper boolean True/False
    
    Functionality added that prevents reposting within a certain threshold

--- BotConfig.py
+++ BotConfig.py
@@ -8,11 +8,17 @@ app_secret = ""
 oauth_token = ""
 oauth_token_secret = ""
 
+#Images Directory, which should look similar to "/var/RedditTwitterPoster/images/"
+img_dir = ""
+
 #This how Reddit sees your script poking in, name it something unique
 user_agent = ""
 
-#Images Directory, which should look similar to "/var/RedditTwitterPoster/images/"
-img_dir = ""
+#Enables if the bot actually posts to twitter. Useful for testing with prints instead of spamming your followers
+enable_tweets = True
+
+#Sets the number of tweets before a submission may be reposted. Recommended no higher than either a quarter of the total link submissions from the subreddit, or 250 (Whichever is smaller) (Integer)
+repeat_threshold = 5
 
 #Choose the subreddit you wish to pull from such as 'python' for reddit.com/r/python
 source_subreddit = 'Python'
@@ -20,32 +26,32 @@ source_subreddit = 'Python'
 #Choose the string of hashtags to use (Recommended no longer than 30 characters) (Set to "" if you want blank)
 hashtags = "#Python #Programming"
 
-#Sets if the bot should try convert strings enclosed by square brackets into hashtags ("enabled" or "disabled")
-autohashtag = "disabled"
+#Sets if the bot should try convert strings enclosed by square brackets into hashtags (True or False)
+autohashtag = False
 
-#Sets if the bot will post the Reddit submission title within the tweet ("enabled" or "disabled")
-post_reddit_title = "enabled"
+#Sets if the bot will post the Reddit submission title within the tweet (True or False)
+post_reddit_title = True
 
-#Sets if the bot will post the Reddit submission permalink within the tweet ("enabled" or "disabled")
-link_to_reddit = "enabled"
+#Sets if the bot will post the Reddit submission permalink within the tweet (True or False)
+link_to_reddit = True
 
-#Sets if the bot will post NSFW-tagged submissions ("enabled" or "disabled")
-post_NSFW = "disabled"
+#Sets if the bot will post NSFW-tagged submissions (True or False)
+post_NSFW = False
 
 #Sets the minimum score a submission must have to be posted (integer)
 min_score = 0
 
-#Sets if the bot will post images (Limited to .png and .jpg/.jpeg) ("enabled" or "disabled")
-post_images = "enabled"
+#Sets if the bot will post images (Limited to .png and .jpg/.jpeg) (True or False)
+post_images = True
 
-#Sets if the bot will post gifs (animated or not) ("enabled" or "disabled")
-post_gifs = "disabled"
+#Sets if the bot will post gifs (animated or not) (True or False)
+post_gifs = False
 
-#Sets if the bot will post links from Vine ("enabled" or "disabled")
-post_vine = "disabled"
+#Sets if the bot will post links from Vine (True or False)
+post_vine = False
 
-#Sets if the bot will post links from Soundcloud ("enabled" or "disabled")
-post_soundcloud = "disabled"
+#Sets if the bot will post links from Soundcloud (True or False)
+post_soundcloud = False
 
-#Sets if the bot will post links from YouTube ("enabled" or "disabled")
-post_youtube = "enabled"
+#Sets if the bot will post links from YouTube (True or False)
+post_youtube = True
--- TwitterRedditPoster.py
+++ TwitterRedditPoster.py
@@ -8,84 +8,118 @@ import os.path
 from twython import Twython
 from mimetypes import guess_extension
 from BotConfig import *
+import urllib3
 
 '''
 IN ORDER TO RUN PROPERLY, THE CONFIGURATION FILE "BotConfig.py" IS NECESSARY
+
+The section below is just temp configs that will be moved into the main config file after functionality testing
 '''
 
-twitter = Twython(app_key,app_secret,oauth_token,oauth_token_secret)
+# end of temp configs
+'''
+def postLink
+
+def postImage
+'''
+# This part opens and loads the previous submissions so it doesn't repeat posts
+file = open(img_dir + "submissions_log.txt", "a+")
+prevlogs = file.read()
+file.close()
+
+twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 r = praw.Reddit(user_agent=user_agent)
 diditpost = 0
 while (diditpost == 0):
-	submission = r.get_random_submission(source_subreddit)
-	if (not submission.over_18) or (post_NSFW == "enabled"):
-		if not submission.is_self:
-			if submission.score > min_score:
-			
-				if link_to_reddit == "enabled":
-					titlelength = 135-(53+len(hashtags))
-					subpermalink = submission.permalink
-				else:
-					titlelength = 135-(23+len(hashtags))
-					subpermalink = ""
-				if autohashtag == "enabled":
-					hashtitle = re.sub(r'(\[)', r'\1#', submission.title)
-					shorttitle = hashtitle[:titlelength] + (hashtitle[titlelength:] and '...')
-				else:
-					shorttitle = submission.title[:titlelength] + (submission.title[titlelength:] and '...')
-				
-				if post_reddit_title == "disabled":
-					shorttitle = ""
-					
-				tweeturl = submission.url
-				if "imgur" in submission.url: 
-					if not ("i.imgur" in submission.url): 
-						iurl = re.sub(r'(\http://)', r'\1i.', submission.url)
-						iexurl = iurl+ ".png";
-					else:
-						iexurl = submission.url
-						iurl = iexurl[:-4]
-					source = urllib.urlopen(iexurl)
-					extension = guess_extension(source.info()['Content-Type'])
-					if extension == ".jpe":
-						tweeturl = iurl+".jpg";
-					elif extension == ".png":
-						tweeturl = iurl+".png";
-					elif extension == ".gif":
-						tweeturl = iurl+".gif";
-					
-					
-				if (tweeturl[-4:] == ".jpg" or tweeturl[-5:] == "jpeg" or tweeturl[-4:] == ".png") and (post_images == "enabled"):
-					if not os.path.exists(img_dir+submission.id+"_"+tweeturl.split('/')[-1]):
-						urllib.urlretrieve(tweeturl, img_dir+submission.id+"_"+tweeturl.split('/')[-1])
-					print submission.title, submission.url, submission.id
-					print "Twitter Pic Post:\n\t" + (shorttitle +" "+ hashtags+" "+ subpermalink)
-					photo = open(img_dir+submission.id+"_"+tweeturl.split('/')[-1], 'rb')
-					twitter.update_status_with_media(status= (shorttitle +" "+ hashtags +" "+ subpermalink), media=photo)
-					diditpost = 1
-								
-				elif (tweeturl[-4:] == ".gif") and (post_gifs == "enabled"):
-					if not os.path.exists(img_dir+submission.id+"_"+tweeturl.split('/')[-1]):
-						urllib.urlretrieve(tweeturl, img_dir+submission.id+"_"+tweeturl.split('/')[-1])
-					print submission.title, submission.url, submission.id
-					print "Twitter Pic Post:\n\t" + (shorttitle +" "+ hashtags+" "+ subpermalink)
-					photo = open(img_dir+submission.id+"_"+tweeturl.split('/')[-1], 'rb')
-					twitter.update_status_with_media(status= (shorttitle +" "+ hashtags +" "+ subpermalink), media=photo)
-					diditpost = 1
-				else:
-					print submission.title, submission.url, submission.id
-					print "link post "+submission.title+" is not jpg, png, or gif"
-					if (("youtube." in submission.url) or ("youtu.be" in submission.url)) and (post_youtube == "enabled"):
-						print "Twitter YouTube Post:\n\t" + (shorttitle +" "+ hashtags+" "+ subpermalink+" "+ tweeturl)
-						twitter.update_status(status=(shorttitle +" "+ hashtags+" "+ subpermalink+" "+ tweeturl))
-						diditpost = 1
-					elif(("vine.co" in submission.url)) and (post_vine == "enabled"):
-						print "Twitter Vine Post:\n\t" + (shorttitle +" "+ hashtags+" "+ subpermalink+" "+ tweeturl)
-						twitter.update_status(status=(shorttitle +" "+ hashtags+" "+ subpermalink+" "+ tweeturl))
-						diditpost = 1
-					elif(("soundcloud" in submission.url) or ("snd.sc" in submission.url)) and (post_soundcloud == "enabled"):
-						print "Twitter Soundcloud Post:\n\t" + (shorttitle +" "+ hashtags+" "+ subpermalink+" "+ tweeturl)
-						twitter.update_status(status=(shorttitle +" "+ hashtags+" "+ subpermalink+" "+ tweeturl))
-						diditpost = 1
-					else:
-						print "[!]There is no Twitter post for this submission try, it will reroll![!]"
+    uniquepost = 0
+    # for submission in r.get_subreddit('niconiconi').get_new(limit=1000):
+    while (uniquepost == 0):
+        submission = r.get_random_submission(source_subreddit)
+        print submission.title, submission.url, submission.id
+        if submission.id in prevlogs:
+            uniquepost = 0
+            print "This submission has already been posted within the threshold, trying for a new submission..."
+        else:
+            uniquepost = 1
+            print "This is a relatively new submission, it will be posted"
+
+            if (not submission.over_18) or (post_NSFW==True):
+                if not submission.is_self:
+                    if submission.score > min_score:
+
+                        if link_to_reddit == True:
+                            titlelength = 135 - (53 + len(hashtags))
+                            subpermalink = submission.permalink
+                        else:
+                            titlelength = 135 - (23 + len(hashtags))
+                            subpermalink = ""
+                        if autohashtag == True:
+                            hashtitle = re.sub(
+                                r'(\[)', r'\1#', submission.title)
+                            shorttitle = hashtitle[
+                                :titlelength] + (hashtitle[titlelength:] and '...')
+                        else:
+                            shorttitle = submission.title[
+                                :titlelength] + (submission.title[titlelength:] and '...')
+
+                        if not post_reddit_title:
+                            shorttitle = ""
+
+                        tweeturl = submission.url
+                        if "imgur" in submission.url:
+                            if not ("i.imgur" in submission.url):
+                                iurl = re.sub(
+                                    r'(\http://)', r'\1i.', submission.url)
+                                iexurl = iurl + ".png"
+                            else:
+                                iexurl = submission.url
+                                iurl = iexurl[:-4]
+                            source = urllib.urlopen(iexurl)
+                            extension = guess_extension(
+                                source.info()['Content-Type'])
+                            if extension == ".jpe":
+                                tweeturl = iurl + ".jpg"
+                            elif extension == ".png":
+                                tweeturl = iurl + ".png"
+                            elif extension == ".gif":
+                                tweeturl = iurl + ".gif"
+
+                        if (tweeturl[-4:] == (".jpg" or tweeturl[-5:] == "jpeg" or tweeturl[-4:] == ".png") and (post_images == True)) or ((tweeturl[-4:] == ".gif") and (post_gifs == True)):
+                            if not os.path.exists(img_dir + submission.id + "_" + tweeturl.split('/')[-1]):
+                                urllib.urlretrieve(
+                                    tweeturl, img_dir + submission.id + "_" + tweeturl.split('/')[-1])
+                            print submission.title, submission.url, submission.id
+                            print "Twitter Pic Post:\n\t" + (shorttitle + " " + hashtags + " " + subpermalink)
+                            photo = open(
+                                img_dir + submission.id + "_" + tweeturl.split('/')[-1], 'rb')
+                            diditpost = 1
+                        else:
+
+                            # print "link post "+submission.title+" is not jpg,
+                            # png, or gif"
+                            if (("youtube." in submission.url) or ("youtu.be" in submission.url)) and (post_youtube == True):
+                                print "Twitter YouTube Post:\n\t" + (shorttitle + " " + hashtags + " " + subpermalink + " " + tweeturl)
+                                diditpost = 1
+                            elif(("vine.co" in submission.url)) and (post_vine == True):
+                                print "Twitter Vine Post:\n\t" + (shorttitle + " " + hashtags + " " + subpermalink + " " + tweeturl)
+                                diditpost = 1
+                            elif(("soundcloud" in submission.url) or ("snd.sc" in submission.url)) and (post_soundcloud == True):
+                                print "Twitter Soundcloud Post:\n\t" + (shorttitle + " " + hashtags + " " + subpermalink + " " + tweeturl)
+                                diditpost = 1
+                            else:
+                                print "[!] The randomly selected submission is not embeddable media! Trying again! [!]"
+                                '''
+                    else:
+                        print "[!]The randomly selected submission doesn't meet the minimum score threshold! Trying again! [!]"
+                 else:
+                    print "[!]The randomly selected submission is a self-post and will not be posted. Trying again! [!]"
+             else:
+                print "[!] The randomly selected submission is NSFW (which is disabled)! Trying again! [!]"'''
+
+# This saves the logs
+file = open(img_dir + "submissions_log.txt", "w+")
+newlogs = submission.id + "\n" + prevlogs
+newlogs = newlogs[:(7*repeat_threshold)]
+print newlogs
+file.write(newlogs)
+file.close()
